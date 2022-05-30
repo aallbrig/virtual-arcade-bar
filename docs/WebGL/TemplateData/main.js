3315886f9647fe00e303ff6fb10804b089bb58be
aallbrig <aallbrig@gmail.com>
2022-05-30 23:30:56 +0000

    Deploying to github-pages from @ aallbrig/virtual-arcade-bar@91a10bc39069cd1c72f153d40503015fd2e6f888 🚀

---
+++ docs/WebGL/TemplateData/main.js
@@ -0,0 +1,14 @@
+var canvas = document.querySelector("#unity-canvas");
+
+var config = {
+    dataUrl: "Build/WebGL.data",
+    frameworkUrl: "Build/WebGL.framework.js",
+    codeUrl: "Build/WebGL.wasm",
+    streamingAssetsUrl: "StreamingAssets",
+    companyName: "Andrew Allbright",
+    productName: "Virtual Arcade Bar",
+    productVersion: "0.1",
+    devicePixelRatio: 1,
+}
+
+createUnityInstance(canvas, config);
