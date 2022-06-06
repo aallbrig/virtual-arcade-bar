9b07ef6e7497d3c07f23ffc7019d3cc6fc17c404
aallbrig <aallbrig@gmail.com>
2022-06-06 00:48:41 +0000

    Deploying to github-pages from @ aallbrig/virtual-arcade-bar@8d2477e76735100362992a831e72e9236a1d23b9 🚀

--- docs/WebGL/TemplateData/main.js
+++ docs/WebGL/TemplateData/main.js
@@ -7,7 +7,7 @@ var config = {
     streamingAssetsUrl: "StreamingAssets",
     companyName: "Andrew Allbright",
     productName: "Virtual Arcade Bar",
-    productVersion: "0.0.20",
+    productVersion: "0.0.21",
     devicePixelRatio: 1,
 }
 
