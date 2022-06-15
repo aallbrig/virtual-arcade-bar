edc0af84016112d832ae036638ab7b0462d4ab68
Andrew Allbright <aallbrig@gmail.com>
2022-06-15 12:18:50 -0400

    Add in reference to docker run command pulled from GH actions

--- scripts/docker-config.sh
+++ scripts/docker-config.sh
@@ -22,7 +22,43 @@ export GITHUB_ACTOR='aallbrig'
 export GITHUB_REPOSITORY='aallbrig/virtual-arcade-bar'
 export CUSTOM_IMAGE_TAG="aallbrig/editor:ubuntu-${UNITY_VERSION}-webgl-${GAME_CI_VERSION}"
 
+
 function Factory::UnityBuilderCommand() {
+# Reference docker run command from GitHub Actions
+# docker run \
+# --workdir /github/workspace \
+# --rm
+# --env UNITY_LICENSE
+# --env UNITY_SERIAL=*HIDDEN*
+# --env UNITY_VERSION=2020.3.35f1
+# --env PROJECT_PATH=unity/virtual-arcade-bar
+# --env BUILD_TARGET=WebGL
+# --env BUILD_NAME=WebGL
+# --env BUILD_PATH=build/WebGL
+# --env BUILD_FILE=WebGL
+# --env VERSION=0.0.34
+# --env ANDROID_VERSION_CODE=34
+# --env GITHUB_REF=refs/heads/main
+# --env GITHUB_SHA=*HIDDEN*
+# --env GITHUB_REPOSITORY=aallbrig/virtual-arcade-bar
+# --env GITHUB_ACTOR=aallbrig
+# --env GITHUB_WORKFLOW=Unity CI CD Pipeline
+# --env GITHUB_EVENT_NAME=push
+# --env GITHUB_WORKSPACE=/github/workspace
+# --env GITHUB_ACTION=builds
+# --env GITHUB_EVENT_PATH=/home/runner/work/_temp/_github_workflow/event.json
+# --env RUNNER_OS=Linux
+# --env RUNNER_TOOL_CACHE=/opt/hostedtoolcache
+# --env RUNNER_TEMP=/home/runner/work/_temp
+# --env RUNNER_WORKSPACE=/home/runner/work/virtual-arcade-bar
+# --env UNITY_SERIAL --env GITHUB_WORKSPACE=/github/workspace
+# --volume /home/runner/work/_temp/_github_home:/root:z
+# --volume /home/runner/work/_temp/_github_workflow:/github/workflow:z
+# --volume /home/runner/work/virtual-arcade-bar/virtual-arcade-bar:/github/workspace:z
+# --volume /home/runner/work/_actions/game-ci/unity-builder/v2/dist/default-build-script:/UnityBuilderAction:z
+# --volume /home/runner/work/_actions/game-ci/unity-builder/v2/dist/platforms/ubuntu/steps:/steps:z
+# --volume /home/runner/work/_actions/game-ci/unity-builder/v2/dist/platforms/ubuntu/entrypoint.sh:/entrypoint.sh:z
+# aallbrig/editor:ubuntu-2020.3.35f1-webgl-1.0.1 /bin/bash -c /entrypoint.sh
   docker run \
      --rm \
      --env UNITY_LICENSE \
